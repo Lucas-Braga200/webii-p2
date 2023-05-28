d89d362bae028552b62dd9851e70009bf11e52ce
Lucas Braga <802.266@alunos.unigran.br>
2023-05-27 21:43:40 -0400

    Ajusta css

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('index.hbs');
+  res.render('index.hbs', { route: 'index' });
 });
 
 router.get('/list', (req, res, next) => {
-  res.render('list.hbs');
+  res.render('list.hbs', { route: 'list' });
+});
+
+router.get('/quiz', (req, res, next) => {
+  res.render('quiz.hbs', { route: 'quiz' });
 });
 
 module.exports = router;
--- server/utils/helpers.js
+++ server/utils/helpers.js
@@ -4,5 +4,8 @@ module.exports = {
       this._sections = {};
     }
     this._sections[name] = options.fn(this);
-  }
+  },
+  ifEquals: function(arg1, arg2, options) {
+    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+  },
 };
